92eb607896cd1fd4aac892a403cfcc444301cfbd
gilzoide <gilzoide@gmail.com>
2020-03-25 20:01:35 -0300

    Add GrammarBuildError, add Rule getters

--- vm/src/grammar.rs
+++ vm/src/grammar.rs
@@ -11,12 +11,21 @@ pub struct Rule<'a> {
     expr: Expression<'a>,
 }
 
+impl<'a> Rule<'a> {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn expr(&self) -> &Expression<'a> {
+        &self.expr
+    }
+}
+
+
 pub struct Grammar<'a> {
     rules: Vec<Rule<'a>>,
     rulemap: HashMap<String, usize>,
 }
-
-pub struct EmptyRuleSet;
 
 impl<'a> Grammar<'a> {
     pub fn new() -> Grammar<'a> {
@@ -31,34 +40,51 @@ impl<'a> Grammar<'a> {
         self.rules.first()
     }
 
-    pub fn add_rule(&mut self, name: &'_ str, expr: Expression<'a>) {
-        let len = self.rules.len();
+    pub fn is_empty(&self) -> bool {
+        self.rules.is_empty()
+    }
+
+    fn add_rule(&mut self, name: &'_ str, expr: Expression<'a>) {
+        let rule_index = self.rules.len();
+        self.rulemap.insert(name.to_string(), rule_index);
         self.rules.push(Rule { name: name.to_string(), expr });
-        self.rulemap.insert(name.to_string(), len);
     }
 }
 
 
-pub struct GrammarBuilder<'a> {
-    grammar: Grammar<'a>,
-    called_rules: HashSet<String>,
+pub enum GrammarBuildError<'a> {
+    EmptyRuleSet,
+    DuplicateRuleName(&'a str),
 }
 
-impl<'a> GrammarBuilder<'a> {
+pub struct GrammarBuilder<'a> {
+    grammar: Grammar<'a>,
+    called_rules: HashSet<&'a str>,
+}
+
+impl<'a, 's> GrammarBuilder<'a> {
     pub fn new() -> GrammarBuilder<'a> {
         GrammarBuilder { grammar: Grammar::new(), called_rules: HashSet::new() }
     }
 
-    pub fn rule(&mut self, name: &'_ str, expr: Expression<'a>) -> &mut Self {
+    pub fn rule(&mut self, name: &'s str, expr: Expression<'a>) -> Result<&mut Self, GrammarBuildError<'s>> {
+        if self.grammar.rulemap.contains_key(name) {
+            return Err(GrammarBuildError::DuplicateRuleName(name))
+        }
         if let Expression::NonTerminal(rule_name) = &expr {
-            self.called_rules.insert(rule_name.to_string());
+            self.called_rules.insert(rule_name);
         }
         self.grammar.add_rule(name, expr);
-        self
+        Ok(self)
     }
 
-    pub fn build(self) -> Grammar<'a> {
-        self.grammar
+    pub fn build(self) -> Result<Grammar<'a>, GrammarBuildError<'s>> {
+        if self.grammar.is_empty() {
+            Err(GrammarBuildError::EmptyRuleSet)
+        }
+        else {
+            Ok(self.grammar)
+        }
     }
 }
 
