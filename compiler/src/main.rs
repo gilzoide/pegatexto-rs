0b07224017e48f261c87a8be81393dcab191f90f
gilzoide <gilzoide@gmail.com>
2020-04-17 16:39:13 -0300

    Arithmetics grammar test

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -5,19 +5,28 @@ use pegatexto_vm::grammar::character_class::CharacterClass;
 use pegatexto_vm::grammar::expression::Expression;
 use pegatexto_vm::matcher::*;
 
-fn test_grammar() -> [(&'static str, Expression); 4] {
+fn test_grammar() -> [(&'static str, Expression); 7] {
     use Expression::*;
 
-	/* CSV <- Line*
-	 * Line <- Field ("," Field)* (EOL / !.)
-	 * Field <- [^\n,]+
-	 * EOL <- \r? \n
-	 */
+	// 
+        // Sp <- \s*
+        // Number <- '-'? \d+ Sp
+        // TermOp <- [+-] Sp
+        // FactorOp <- [*/] Sp
+        // 
+        // Exp <- Term (TermOp Term)*
+        // Term <- Factor (FactorOp Factor)*
+        // Factor <- Number / '(' Sp Exp ')' Sp
+        //
     [
-        ("CSV", NonTerminal("Line".to_string())^0),
-        ("Line", (NonTerminal("Field".to_string()) + ((Char(',') + NonTerminal("Field".to_string()))^0) + (NonTerminal("EOL".to_string()) / !Any)) >> "line"),
-        ("Field", (InverseSet("\n,".to_string())^1) >> "field"),
-        ("EOL", (Char('\r')^(-1)) + Char('\n')),
+        ("Exp", NonTerminal("Term".to_string()) + ((NonTerminal("TermOp".to_string()) + NonTerminal("Term".to_string()))^0)),
+        ("Term", NonTerminal("Factor".to_string()) + ((NonTerminal("FactorOp".to_string()) + NonTerminal("Factor".to_string()))^0)),
+        ("Factor", NonTerminal("Number".to_string()) / (Char('(') + NonTerminal("Sp".to_string()) + NonTerminal("Exp".to_string()) + Char(')') + NonTerminal("Sp".to_string()))),
+
+        ("Sp", Class(CharacterClass::Whitespace)^0),
+        ("Number", (Char('-')^(-1)) + (Class(CharacterClass::Digit)^1) + NonTerminal("Sp".to_string())),
+        ("TermOp", Set("+-".to_string()) + NonTerminal("Sp".to_string())),
+        ("FactorOp", Set("*/".to_string()) + NonTerminal("Sp".to_string())),
     ]
 }
 
@@ -28,7 +37,7 @@ fn main() {
     let bytecode = compiler.emit();
     dump_bytecode(&bytecode);
 
-    let result = try_match_then(&bytecode, "oi,cábra\nda,peste", |s, i, args| {
+    let result = try_match_then(&bytecode, "3 + 5*9 / (1+1) - 12", |s, i, args| {
         println!("!! ACTION {:?} {} {}", s, i, args.len());
         args.iter().max().copied().unwrap_or(s.len())
     });
